dbe3461ea5be31748343fadc57dbcb1bfda5345e
jixulin0815 <1771728914@qq.com>
2020-12-02 21:25:49 +0800

    交

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -51,7 +51,7 @@ const user = {
             reject('getInfo: roles must be a non-null array !')
           }
           commit('SET_NAME', data.name)
-          commit('SET_AVATAR', data.avatar)
+          commit('SET_AVATAR', data.avatar) //用户头像
           resolve(response)
         }).catch(error => {
           reject(error)
